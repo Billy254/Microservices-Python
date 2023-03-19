e0dff6e3d8122394af82ca974d5740ada2a25020
Billy254 <34514031+Billy254@users.noreply.github.com>
2023-03-19 15:09:22 +0000

    add cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,8 @@ def wiki(name="War Goodness", length=1):
     """Wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """Search wikipedia for Names"""
+    results = wikipedia.search(name)
+    return results
