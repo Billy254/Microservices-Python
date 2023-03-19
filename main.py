a6ff0755b0c6e2c88510aa1f80c41c270dd83225
Billy254 <34514031+Billy254@users.noreply.github.com>
2023-03-19 16:01:20 +0000

    adding fast api

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import wiki as wikilogic
+from mylib.logic import search_wiki
 
-print(wiki())
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API: call /search or /wiki"}
+
+@app.get("/search/{value}")
+async def search(value : str):
+    """Page to search in wikipedia"""
+    result = search_wiki(value)
+    return {"result": result}
+
+@app.get("/wiki/{name}")
+async def wiki(name : str):
+    """Page to search in wikipedia"""
+    result = wikilogic(name)
+    return {"result": result}
+    
+if __name__ == "__main__":
+    uvicorn.run(app, port=8082, host="0.0.0.0")
